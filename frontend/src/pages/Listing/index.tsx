8d502d9ae8f737a0073afa9ec5b6cb8178c1c9f6
Joyce Fernandes <joyce_f.s@hotmail.com>
2022-05-14 11:28:32 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,15 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data)
+        });
+
     return (
         <>
             <Pagination />
@@ -10,9 +18,6 @@ function Listing() {
                 <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
-                    </div> 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
@@ -22,7 +27,10 @@ function Listing() {
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
-                    </div>                   
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
                 </div>
             </div>
         </>
